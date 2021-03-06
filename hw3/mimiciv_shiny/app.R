a51c12c837d92cde9984469aa9de3bc5319a108f
Valentina Arputhasamy <valentina214@server.ucla-biostat-203b.com>
2021-03-06 06:25:30 +0000

    finished third tab

--- hw3/mimiciv_shiny/app.R
+++ hw3/mimiciv_shiny/app.R
@@ -19,14 +19,6 @@ icu_cohort <- readRDS("icu_cohort.rds")
 demtable <- icu_cohort %>%
   select(subject_id, gender, age_at_adm, ethnicity, language, marital_status )
 
-labtable <- icu_cohort %>%
-  select(bicarbonate, calcium, chloride, creatinine, glucose, magnesium,
-         potassium, sodium, hematocrit, lactate)
-
-colnames(labtable) <- c("Bicarbonate", "Calcium", "Chloride", 
-                        "Creatinine", "Glucose", "Magnesium", 
-                        "Potassium", "Sodium", "Hematocrit", "Lactate")
-
 demtable$language <- ifelse(demtable$language == "?", "Unknown", "English" )
 
 colnames(demtable) <- c("Subject ID", "Gender", "Age (years)", "Ethnicity", 
@@ -37,6 +29,42 @@ demtable$Ethnicity <- as.factor(demtable$Ethnicity)
 demtable$Language <- as.factor(demtable$Language)
 demtable$`Marital Status` <- as.factor(demtable$`Marital Status`)
 
+labtable <- icu_cohort %>%
+  select(subject_id, bicarbonate, calcium, chloride, creatinine, glucose, magnesium,
+         potassium, sodium, hematocrit, lactate)
+
+colnames(labtable) <- c("Subject ID", "Bicarbonate", "Calcium", "Chloride", 
+                        "Creatinine", "Glucose", "Magnesium", 
+                        "Potassium", "Sodium", "Hematocrit", "Lactate")
+
+labdemtable <- icu_cohort %>%
+  select(gender, age_at_adm, ethnicity, language, marital_status, bicarbonate,
+         calcium, chloride, creatinine, glucose, magnesium,
+         potassium, sodium, hematocrit, lactate)
+
+colnames(labdemtable) <- c("Gender", "Age (Years)", "Ethnicity", "Language", 
+                           "Marital Status", "Bicarbonate", "Calcium", "Cholride",
+                           "Creatinine", "Glucose", "Magnesium", "Potassium",
+                           "Sodium","Hematocrit","Lactate")
+
+vitaldemtable <- icu_cohort %>%
+  select(gender, age_at_adm, ethnicity, language, marital_status, heart_rate, 
+         non_invasive_blood_pressure_systolic, non_invasive_blood_pressure_mean,
+         respiratory_rate, temperature_fahrenheit, arterial_blood_pressure_systolic,
+         arterial_blood_pressure_mean)
+
+colnames(vitaldemtable) <- c("Gender", "Age (Years)", "Ethnicity", "Language", 
+                             "Marital Status", "Heart Rate", "Non-Invasive Systolic BP",
+                             "Mean Non-Invasive BP", "Respiratory Rate",
+                             "Temperature (Fahrenheit)", "Systolic Arterial BP", 
+                             "Mean Arterial BP")
+
+labdemtable$Gender <- as.factor(demtable$Gender)
+labdemtable$Ethnicity <- as.factor(demtable$Ethnicity)
+labdemtable$Language <- as.factor(demtable$Language)
+labdemtable$`Marital Status` <- as.factor(demtable$`Marital Status`)
+
+
 #fluid page - what the app looks like
 
 # Define UI 
@@ -46,7 +74,7 @@ ui <- fluidPage(
   titlePanel("MIMIC-IV Data Visualization"),
   
   navbarPage("MIMIC Data", theme = shinytheme("lumen"),
-             tabPanel("Demographics", #first tab for demographics
+             tabPanel("Demographics", fluid = T, #first tab for demographics
                  titlePanel("Demographic Charts"),
              sidebarLayout(
                sidebarPanel(
@@ -76,23 +104,82 @@ ui <- fluidPage(
                     mainPanel(
                       tableOutput("view")))),
   
-              tabPanel("Lab Measurements",
-                       titlePanel("Lab Measurement Distributions"),
+              tabPanel("Lab Measurements", fluid = T,
+                       titlePanel("Distributions"),
                        sidebarLayout(
                          sidebarPanel(
                          radioButtons("labchoice", "Select Lab Measurement:", c("Bicarbonate", "Calcium", "Creatinine", "Glucose", "Magnesium", "Potassium",
                                                      "Sodium", "Hematocrit", "Lactate")),
                          numericInput("obslab", "Number of observations (max: 50,048)", 25000, max = 50048)),
                        mainPanel(
-                         plotOutput("labplot"))))))
-  
+                         plotOutput("labplot"))),
+                       
+                       titlePanel("Summary Statistics"),
+                       sidebarLayout(
+                         sidebarPanel(
+                           selectInput("labsumm", "Select Lab Type:", c("Bicarbonate", "Calcium", "Creatinine", "Glucose", "Magnesium",
+                                                                        "Sodium", "Lactate"))),
+                         mainPanel(
+                           verbatimTextOutput("labsumtable"))),
+                       
+                       titlePanel("Lab Measurements by Age, by Demographic"),
+                       sidebarLayout(
+                         sidebarPanel(
+                           selectInput("labtype", "Select Lab Type:", c("Bicarbonate", "Calcium", "Creatinine", "Glucose", "Magnesium",
+                                                                        "Sodium", "Lactate")),
+                           selectInput("demtype", "Select Demographic Type:", c("Gender", "Ethnicity", "Language", "Marital Status"))),
+                         mainPanel(plotOutput("facetplot")))),
+                       
+                  tabPanel("Vital Measurements", fluid = T,
+                           titlePanel("Distributions"),
+                           sidebarLayout(
+                             sidebarPanel(
+                               radioButtons("vitaltype", "Select Vital Type:", 
+                                            c("Heart Rate", "Non-Invasive Systolic BP", 
+                                              "Mean Non-Invasive BP",
+                                              "Respiratory Rate", "Temperature (Fahrenheit)",
+                                              "Systolic Arterial BP", "Mean Arterial BP")),
+                               numericInput("obsvital", "Number of observations (max: 50,048)", 25000, max = 50048)),
+                             mainPanel(plotOutput("vitalplot"))),
+                           
+                           titlePanel("Summary Statistics"),
+                           sidebarLayout(
+                             sidebarPanel(
+                               selectInput("vitalsumm", "Select Vital Type:",
+                                           c("Heart Rate", "Non-Invasive Systolic BP", "Mean Non-Invasive BP",
+                                             "Mean Non-Invasive BP", "Respiratory Rate",
+                                             "Temperature (Fahrenheit)", "Systolic Arterial BP", "Mean Arterial BP",
+                                             "Systolic Arterial BP", "Mean Arterial BP"))),
+                           mainPanel(
+                             verbatimTextOutput("vitalsumtable"))),
+                           
+                           titlePanel("Vital Measurements by Age, by Demographic"),
+                           sidebarLayout(
+                             sidebarPanel(
+                               selectInput("vitaltype2", "Select Vital Type:", 
+                                           c("Heart Rate", "Non-Invasive Systolic BP","Mean Non-Invasive BP",
+                                             "Respiratory Rate", "Temperature (Fahrenheit)", "Systolic Arterial BP",
+                                             "Mean Arterial BP", "Systolic Arterial BP", "Mean Arterial BP")),
+                               selectInput("demtype2", "Select Demographic Type:", 
+                                           c("Gender", "Ethnicity", "Language", "Marital Status"))),
+                             mainPanel(plotOutput("facetplot2"))))))
+          
+                        
+                           
+                           
+                          
+                       
+            
+
+
+
 
 
 server <- function(input, output) {
   
     output$demPlot <- renderPlot({
       ggplot(demtable, aes(eval(as.name(input$demchoice)))) + 
-        geom_bar(fill = "lightsteelblue2", color = "lightsteelblue2" ) + 
+        geom_bar(fill = "darkseagreen3", color = "darkseagreen4" ) + 
         coord_flip() + xlab(input$demchoice)
     })
     
@@ -117,14 +204,37 @@ server <- function(input, output) {
       
     output$labplot <- renderPlot({
       ggplot(labtable, aes(eval(as.name(input$labchoice)))) + 
-        geom_histogram(binwidth = 1, fill="lightsteelblue2", color = "lightsteelblue4") +
+        geom_histogram(binwidth = 1, fill="darkseagreen3", color = "darkseagreen4") +
         xlab(input$labchoice) 
     })
     
+    output$labsumtable <- renderPrint({
+      labtable %>% select(input$labsumm) %>% summary()
+      
+    })
+    
+    output$facetplot <- renderPlot({
+      ggplot(labdemtable) + geom_point(mapping = aes(x = `Age (Years)`, y = eval(as.name(input$labtype))), color = "darkseagreen4") +
+        facet_wrap(~eval(as.name(input$demtype))) + ylab(input$labtype)
         
+    })
+    
+    output$vitalplot <- renderPlot({
+      ggplot(vitaldemtable, aes(eval(as.name(input$vitaltype)))) + 
+        geom_histogram(binwidth = 1, fill="darkseagreen3", color = "darkseagreen4") +
+        xlab(input$vitaltype) 
+    })
+    
+    output$vitalsumtable <- renderPrint({
+      vitaldemtable %>% select(input$vitalsumm) %>% summary()
+    })
+    
+    output$facetplot2 <- renderPlot({
+      ggplot(vitaldemtable) + geom_point(mapping = aes(x = `Age (Years)`, y = eval(as.name(input$vitaltype2))), color = "darkseagreen4") +
+        facet_wrap(~eval(as.name(input$demtype2))) + ylab(input$vitaltype2)
+    })
   
 }
-
 
 
 
